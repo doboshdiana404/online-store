da11dbb88f4662395eff5815b4f5e50192284e3d
diana_dobosh <diana.lakatosh.girl@gmail.com>
2025-03-29 23:27:57 +0200

    init Speed Insights

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,12 @@ import ReactDOM from 'react-dom/client';
 import 'modern-normalize';
 import App from './App';
 import './index.css';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+reportWebVitals(console.log);
