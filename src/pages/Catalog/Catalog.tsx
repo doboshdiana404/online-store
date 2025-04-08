3ebff11c7738123595d4ea382ff0aa9068cf78b9
Taras Kazymir <kazemir.t23.23@gmail.com>
2025-04-08 16:15:44 +0300

    feat: add product api and catalog page

--- src/pages/Catalog/Catalog.tsx
+++ src/pages/Catalog/Catalog.tsx
@@ -1,7 +1,54 @@
+import { Link, useSearchParams } from 'react-router-dom';
+
+import PageSizeSelect from '@/modules/Pagination/PageSizeSelect/PageSizeSelect';
+import Pagination from '@/modules/Pagination/Pagination';
+
 import styles from './Catalog.module.css';
 
+import image from '@/assets/example/example.png';
+import { useGetAllProductsQuery } from '@/redux/services/products';
+
 const Catalog = () => {
-  return <div className={styles.catalog}>Catalog</div>;
+  const [searchParams] = useSearchParams();
+  const pageNumber = searchParams.get('page') || 1;
+  const pageSize = searchParams.get('size') || 8;
+  const {
+    data: products,
+    isSuccess,
+    isLoading,
+    isFetching,
+  } = useGetAllProductsQuery({ pageSize, pageNumber });
+  return (
+    <section className={styles.catalog}>
+      <h2>Catalog</h2>
+      <div>
+        <PageSizeSelect />
+      </div>
+      <div className={styles['product-list']}>
+        {isSuccess &&
+          products.items.map(({ id, ...product }) => (
+            <Link
+              to={`/product/${id}`}
+              key={id}
+              className={styles['product-card']}
+            >
+              <img src={image} alt="" loading="lazy" width={250} height={250} />
+              <h3>{product.name}</h3>
+              <p>{product.sortDescription}</p>
+              <p>{product.price}$</p>
+            </Link>
+          ))}
+        {isFetching || (isLoading && <p>Loading...</p>)}
+      </div>
+      {isSuccess && (
+        <Pagination
+          skip={products.skip}
+          take={products.take}
+          totalItems={products.totalItems}
+        />
+      )}
+    </section>
+  );
 };
 
 export default Catalog;
---
+++ src/redux/services/products.ts
@@ -0,0 +1,103 @@
+import { baseApi } from './baseApi';
+
+// TODO: update interface Products
+export interface Product {
+  id: string;
+  name: string;
+  imageUrl: string;
+  sortDescription: string;
+  price: number;
+  isActive: boolean;
+}
+
+export interface ProductsList {
+  items: Product[];
+  totalItems: number;
+  skip: number;
+  take: number;
+}
+
+export interface ProductsListArgs {
+  searchQuery?: string;
+  categoryId?: string;
+  isActive?: boolean;
+  minPrice?: string;
+  maxPrice?: string;
+  pageNumber?: number | string;
+  pageSize?: number | string;
+  sortBy?: 'price' | 'rating';
+  sortDirection?: 'asc' | 'desc';
+}
+
+export interface ProductById {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  imageUrl: string;
+  sku: string;
+  rating: number;
+  stockQuantity: number;
+  isActive: boolean;
+  reviews: [];
+  categoryId: string;
+}
+
+export interface ProductCreate {
+  name: string;
+  description: string;
+  price: number;
+  imageUrl: string;
+  sky: string;
+  stockQuantity: string;
+  categoryId: string;
+  isActive: boolean;
+}
+
+export const categoryApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    getAllProducts: builder.query<ProductsList, ProductsListArgs>({
+      query: ({ pageNumber = 1, pageSize = 4 }) => ({
+        url: `/products?PageNumber=${pageNumber}&PageSize=${pageSize}`,
+      }),
+      providesTags: ['Product'],
+    }),
+    getProductById: builder.query<ProductById, string>({
+      query: (id) => ({
+        url: `/products/${id}`,
+      }),
+    }),
+    deleteProduct: builder.mutation<string, string>({
+      query: (id) => ({
+        url: `/products/${id}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: ['Product'],
+    }),
+    createProduct: builder.mutation<Product, ProductCreate>({
+      query: (body) => ({
+        url: `/categories`,
+        method: 'POST',
+        body,
+      }),
+      invalidatesTags: ['Product'],
+    }),
+    editProduct: builder.mutation<Product, Product>({
+      query: ({ id, ...body }) => ({
+        url: `/categories/${id}`,
+        method: 'PUT',
+        body,
+      }),
+      invalidatesTags: ['Product'],
+    }),
+  }),
+  overrideExisting: false,
+});
+
+export const {
+  useGetProductByIdQuery,
+  useGetAllProductsQuery,
+  useCreateProductMutation,
+  useDeleteProductMutation,
+  useEditProductMutation,
+} = categoryApi;
