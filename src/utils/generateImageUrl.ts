0d71f10552a5796dd37e844f728b78603dadf42b
Taras Kazymir <kazemir.t23.23@gmail.com>
2025-04-23 10:00:07 +0300

    feat: add utils function

---
+++ src/utils/generateImageUrl.ts
@@ -0,0 +1,8 @@
+const baseURLApi = import.meta.env.VITE_API_BASE_URL;
+export const generateImageUrl = (
+  id: string,
+  type: 'mobile' | 'tablet' | 'desktop'
+) => {
+  const imageId = id.endsWith('.webp') ? id.slice(0, -5) : id;
+  return `${baseURLApi}images/${imageId}-${type}.webp`;
+};
