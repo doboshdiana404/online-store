140043114d081f069ec02991e43fc09872a12986
Taras Kazymir <kazemir.t23.23@gmail.com>
2025-05-22 17:56:32 +0300

    feat: add Banner modules

---
+++ src/pages/Home/components/BannerIcons/BannerIcons.tsx
@@ -0,0 +1,136 @@
+import styles from './BannerIcons.module.css';
+
+const BannerIcons = () => {
+  return (
+    <>
+      <img
+        className={styles.box}
+        src="/banner/icons/candy__box.svg"
+        width={368}
+        height={368}
+        alt="icon box"
+      />
+      <img
+        className={styles['icon_1']}
+        src="/banner/icons/icon_1.svg"
+        width={120}
+        height={120}
+        alt="icon box"
+      />
+      <img
+        className={styles['icon_2']}
+        src="/banner/icons/icon_2.svg"
+        width={84}
+        height={104}
+        alt="icon box"
+      />
+      <img
+        className={styles['icon_3']}
+        src="/banner/icons/icon_3.svg"
+        width={135}
+        height={130}
+        alt="icon box"
+      />
+      <img
+        className={styles['icon_4']}
+        src="/banner/icons/icon_4.svg"
+        width={100}
+        height={100}
+        alt="icon box"
+      />
+      <img
+        className={styles['icon_5']}
+        src="/banner/icons/icon_5.svg"
+        width={90}
+        height={90}
+        alt="icon box"
+      />
+      <img
+        className={styles['icon_6']}
+        src="/banner/icons/icon_6.svg"
+        width={110}
+        height={95}
+        alt="icon box"
+      />
+      <img
+        className={styles['icon_7']}
+        src="/banner/icons/icon_7.svg"
+        width={160}
+        height={175}
+        alt="icon box"
+      />
+      <img
+        className={styles['icon_8']}
+        src="/banner/icons/icon_8.svg"
+        width={100}
+        height={100}
+        alt="icon box"
+      />
+      <img
+        className={styles['icon_9']}
+        src="/banner/icons/icon_9.svg"
+        width={100}
+        height={80}
+        alt="icon box"
+      />
+      <img
+        className={styles['icon_10']}
+        src="/banner/icons/icon_10.svg"
+        width={135}
+        height={150}
+        alt="icon box"
+      />
+      <img
+        className={styles['icon_11']}
+        src="/banner/icons/icon_11.svg"
+        width={70}
+        height={70}
+        alt="icon box"
+      />
+      <img
+        className={styles['icon_12']}
+        src="/banner/icons/icon_12.svg"
+        width={100}
+        height={90}
+        alt="icon box"
+      />
+      <img
+        className={styles['icon_13']}
+        src="/banner/icons/icon_13.svg"
+        width={100}
+        height={115}
+        alt="icon box"
+      />
+      <img
+        className={styles['icon_14']}
+        src="/banner/icons/icon_14.svg"
+        width={80}
+        height={80}
+        alt="icon box"
+      />
+      <img
+        className={styles['icon_15']}
+        src="/banner/icons/icon_15.svg"
+        width={100}
+        height={95}
+        alt="icon box"
+      />
+      <img
+        className={styles['icon_16']}
+        src="/banner/icons/icon_16.svg"
+        width={100}
+        height={110}
+        alt="icon box"
+      />
+      <img
+        className={styles['icon_17']}
+        src="/banner/icons/icon_17.svg"
+        width={125}
+        height={125}
+        alt="icon box"
+      />
+    </>
+  );
+};
+
+export default BannerIcons;
---
+++ src/pages/Home/modules/Banner/Banner.tsx
@@ -0,0 +1,47 @@
+import Link from '@/ui/Link/Link';
+
+import BannerIcons from '../../components/BannerIcons/BannerIcons';
+
+import styles from './Banner.module.css';
+
+import { Header } from '@/Layout/Header/Header';
+
+const Banner = () => {
+  return (
+    <section className={styles.banner}>
+      <Header />
+      <div className="container">
+        <section className={styles.title}>
+          <h1>
+            <p>Seasonal Treats up</p>
+            <span>20% OFF</span>
+          </h1>
+          <Link variant="primary" to="/shop">
+            Shop now
+          </Link>
+          <div className={styles.delivery}>
+            <img
+              src="/banner/delivery.svg"
+              alt="delivery free from 2000 uah"
+              width={64}
+              height={64}
+            />
+            <p>Free delivery from 2000 UAН</p>
+          </div>
+        </section>
+      </div>
+      <a href="#category">
+        <img
+          className={styles.down}
+          src="/controls.svg"
+          width={36}
+          height={36}
+          alt="arrow down"
+        />
+      </a>
+      <BannerIcons />
+    </section>
+  );
+};
+
+export default Banner;
