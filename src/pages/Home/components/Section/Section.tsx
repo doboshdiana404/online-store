b06074e5bf8130503957c7503c055bfe6e25bd0e
Taras Kazymir <kazemir.t23.23@gmail.com>
2025-05-22 18:05:22 +0300

    feat: add NewArrivals and Bestsellers modules

--- src/pages/Home/components/Section/Section.tsx
+++ src/pages/Home/components/Section/Section.tsx
@@ -5,12 +5,13 @@ import styles from './Section.module.css';
 export interface SectionProps {
   children: ReactNode;
   text: string;
+  id?: string;
 }
 
-const Section: FC<SectionProps> = ({ children, text }) => {
+const Section: FC<SectionProps> = ({ children, text, id }) => {
   return (
     <div className="container">
-      <section className={styles.section}>
+      <section className={styles.section} id={id}>
         <h2 className={styles.title}>{text}</h2>
         {children}
       </section>
---
+++ src/pages/Home/modules/Bestsellers/Bestsellers.tsx
@@ -0,0 +1,19 @@
+import Slider from '@/modules/Slider/Slider';
+
+import ProductCard from '../../components/ProductCard/ProductCard';
+
+import { useGetBestsellersProductQuery } from '@/redux/services/products';
+
+const Bestsellers = () => {
+  const { data: bestsellers, isSuccess } = useGetBestsellersProductQuery();
+  return (
+    <Slider options={{ loop: true, startIndex: 0 }}>
+      {isSuccess &&
+        bestsellers.map((bestsellers) => (
+          <ProductCard key={bestsellers.id} {...bestsellers} />
+        ))}
+    </Slider>
+  );
+};
+
+export default Bestsellers;
