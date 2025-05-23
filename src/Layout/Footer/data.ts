650f6f2b0fbfa64ed3028974829ca1f4639c7f1f
Taras Kazymir <kazemir.t23.23@gmail.com>
2025-05-23 11:14:57 +0300

    feat: add Footer modules

---
+++ src/Layout/Footer/data.ts
@@ -0,0 +1,17 @@
+export const FOOTER_ICONS = [
+  {
+    id: 1,
+    to: '/',
+    icon: '/footer/facebook.svg',
+  },
+  {
+    id: 2,
+    to: '/',
+    icon: '/footer/instagram.svg',
+  },
+  {
+    id: 3,
+    to: '/',
+    icon: '/footer/twitter.svg',
+  },
+];
