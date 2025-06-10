96a21655c8bed6e3980ad1b76c9393d655d7719e
Taras Kazymir <kazemir.t23.23@gmail.com>
2025-06-10 12:41:04 +0300

    feat: add Catalog page

--- src/pages/Catalog/Catalog.tsx
+++ src/pages/Catalog/Catalog.tsx
@@ -1,75 +1,68 @@
+import { useEffect } from 'react';
+
 import { useSearchParams } from 'react-router-dom';
 
-import ResetButton from '@/pages/Catalog/modules/ClearFilters/ClearFilters';
-import PageSizeSelect from '@/pages/Catalog/modules/PageSizeSelect/PageSizeSelect';
-
-import CategoriesList from '@/modules/CategoriesList/CategoriesList';
 import Pagination from '@/modules/Pagination/Pagination';
 
 import ProductCard from '@/components/ProductCard/ProductCard';
 
 import styles from './Catalog.module.css';
-import PriceRangeFilter from './modules/PriceRangeFilter/PriceRangeFilter';
-import Search from './modules/Search/Search';
-import SortByIsActive from './modules/SortByIsActive/SortByIsActive';
-import SortSelect from './modules/SortSelect/SortSelect';
+import Filters from './modules/Filters/Filters';
+import ProductList from './modules/ProductList/ProductList';
 
 import { useGetAllProductsQuery } from '@/redux/services/products';
 
 const Catalog = () => {
   const [searchParams] = useSearchParams();
   const pageNumber = searchParams.get('page') || 1;
-  const pageSize = searchParams.get('size') || 8;
   const categoryId = searchParams.get('CategoryId') || '';
   const sortBy = searchParams.get('SortBy') || '';
+  const pageSize = searchParams.get('size') || 8;
   const sortDirection = searchParams.get('SortDirection') || '';
   const searchQuery = searchParams.get('SearchQuery') || '';
   const maxPrice = searchParams.get('MaxPrice') || '';
   const minPrice = searchParams.get('MinPrice') || '';
   const isActive = searchParams.get('IsActive') || '';
 
-  const {
-    data: products,
-    isSuccess,
-    isLoading,
-    isFetching,
-  } = useGetAllProductsQuery({
-    pageSize,
+  const { data: products, isSuccess } = useGetAllProductsQuery({
     pageNumber,
     categoryId,
     sortBy,
+    pageSize,
     sortDirection,
     searchQuery,
     maxPrice,
     minPrice,
     isActive,
   });
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }, [products]);
   return (
     <section className={styles.catalog}>
-      <h2>Catalog</h2>
-      <Search />
-      <CategoriesList />
-      <div className={styles.filters}>
-        <PageSizeSelect />
-        <SortSelect />
-        <PriceRangeFilter />
-        <SortByIsActive />
-        <ResetButton />
+      <div className="container">
+        <div className={styles.wrapper}>
+          <section className={styles['bread-crams']}>Main / Shop</section>
+          <main>
+            <Filters />
+            {isSuccess && (
+              <ProductList>
+                {products.items.map((product) => (
+                  <ProductCard key={product.id} {...product} />
+                ))}
+              </ProductList>
+            )}
+          </main>
+          {isSuccess && (
+            <Pagination
+              skip={products.skip}
+              take={products.take}
+              totalItems={products.totalItems}
+            />
+          )}
+        </div>
       </div>
-      <div className={styles['product-list']}>
-        {isSuccess &&
-          products.items.map((product) => (
-            <ProductCard key={product.id} {...product} />
-          ))}
-        {isFetching || (isLoading && <p>Loading...</p>)}
-      </div>
-      {isSuccess && (
-        <Pagination
-          skip={products.skip}
-          take={products.take}
-          totalItems={products.totalItems}
-        />
-      )}
     </section>
   );
 };
